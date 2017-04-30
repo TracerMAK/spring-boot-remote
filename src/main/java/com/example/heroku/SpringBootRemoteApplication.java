d0b2ae9a43b326f8dedcb74a80d84e661a344a42
TracerMAK <tsmckiel@live.com>
2017-04-29 22:11:53 -0500

    first commit

---
+++ src/main/java/com/example/heroku/SpringBootRemoteApplication.java
@@ -0,0 +1,12 @@
+package com.example.heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRemoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRemoteApplication.class, args);
+	}
+}
